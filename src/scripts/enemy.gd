7219c3d133a6f11f3ef8c932c680e702d74a050f
andre vargas <vargasfandre@gmail.com>
2025-07-12 11:22:21 -0300

    player nome

--- src/scripts/enemy.gd
+++ src/scripts/enemy.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 
-var player
+@onready var player = %Player
 @export var speed = 50
 
-func _ready():
-	player = get_node("../Player")
+func _process(delta):
+	if !player:
+		player = get_tree().get_root().find_child("Player", true, false)
+		if player:
+			print("Player encontrado DEPOIS:", player)
 
 func _physics_process(delta):
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * speed
-	move_and_slide()
+	if player:
+		var direction = global_position.direction_to(player.global_position)
+		velocity = direction * speed
+		move_and_slide()
