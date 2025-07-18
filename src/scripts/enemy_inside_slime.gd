002dd236a4089e0f126c2a4f3cf09aca2d102e04
andre vargas <vargasfandre@gmail.com>
2025-07-17 21:29:21 -0300

    Inimigo ligeiramente transparente e verdinho

--- src/scripts/enemy_inside_slime.gd
+++ src/scripts/enemy_inside_slime.gd
@@ -3,7 +3,14 @@ extends Node2D
 
 @export var rigidbody: RigidBody2D
 @export var energy: float
+@onready var sprite = $RigidBody2D/Sprite2D
 
+
+func _ready():
+	apply_slime_tint()
+
+func apply_slime_tint():
+	sprite.modulate = Color(0.6, 1.0, 0.6, 0.3)  # verde claro
 
 func _physics_process(delta):
 	if randi() % 60 == 0:
